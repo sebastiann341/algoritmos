27f2164979b0f9d2f6927139e4b125b1398fcaed
sebastiann341 <144932630+sebastiann341@users.noreply.github.com>
2023-09-22 10:46:22 -0500

    Add files via upload

---
+++ #ejercicio 9.py
@@ -0,0 +1,31 @@
+#ejercicio 9
+ 
+#Vamos a realizar un program que realice una cupla compuesta por diferentes tipos de datos que incluyan variables adcionalmente que esten orientados a realizar diferentes acciones
+#Enfocadas en generar resultados en esas variaciones
+
+#print("Este es un programa de tuplas")
+
+#n = [{1,4},{4,8}]
+#t = [{"Sebas",1},{34,"Hogar"}]
+
+#print(n,t)
+
+#res = ()
+
+#for n in res:
+    #print(n)
+    #if  n == res:
+        #print(res)
+
+#Localizar desntro un tupla la posicion determinda por un operador de referencia como el . -0-1     2-4       3-9
+ 
+A = ("Casa", "Programa para posiiconar", "tuplas en una variable", [2023] ,"Perro", [2,9,2022], "Computadora", "moto", "Phyton")
+
+print(A[0:1])
+print(A[2:4])
+print(A[3:9])
+
+#for A in True:
+    #print(A)
+    #if A == True:
+        #print("Casa")
